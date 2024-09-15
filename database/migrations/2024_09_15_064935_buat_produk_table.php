bda371003cffb6b89f24a9876efe21309ca7f5fb
Madeline Emily <93426401+madelinemily@users.noreply.github.com>
2024-09-15 14:19:50 +0700

    feat: updated

---
+++ database/migrations/2024_09_15_064935_buat_produk_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id_produk');
+            $table->unsignedInteger('id_kategori');
+            $table->string('kode_produk')->unique();
+            $table->string('nama_produk')->unique();
+            $table->string('merk')->nullable();
+            $table->integer('harga_beli');
+            $table->tinyInteger('diskon')->default(0);
+            $table->integer('harga_jual');
+            $table->integer('stok');
+            $table->timestamps();
+
+            $table->foreign('id_kategori')
+                  ->references('id_kategori')
+                  ->on('kategori')
+                  ->onUpdate('restrict')
+                  ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produk', function (Blueprint $table) {
+            // Drop foreign key constraint
+            $table->dropForeign(['id_kategori']);
+        });
+
+        Schema::dropIfExists('produk');
+    }
+};
